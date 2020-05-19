25f28d3af2f95446a34104a3092e8210b2a504d9
lavanya2121 <lavanya11289@gmail.com>
2020-05-19 22:22:59 +0530

    initial commit

---
+++ server/users.js
@@ -0,0 +1,43 @@
+const users=[];
+
+//helper functions
+
+const addUser=({id,name,room})=>{
+    //Javascript Mastery=javascriptmastery
+
+    name=name.trim().toLowerCase();
+    room=room.trim().toLowerCase();
+
+    //existing user
+    const existingUser=users.find((user)=>user.room===room && user.name===name)
+
+    if(existingUser){
+        return {error:'Username is taken'}
+    }
+
+    //creating a new user
+    const user={id,name,room};
+
+    users.push(user);
+
+    return {user};
+
+}
+
+const removeUser=(id)=>{
+    const index=users.findIndex((user)=>user.id===id);
+
+    if(index!==-1){
+        return users.splice(index,1)[0];
+    }
+
+}
+
+const getUser=(id)=>users.find((user)=>user.id===id);
+
+
+const getUsersInRoom=(room)=>users.filter((user)=>user.room===room);
+
+module.exports={addUser,removeUser,getUser,getUsersInRoom};
+
+
